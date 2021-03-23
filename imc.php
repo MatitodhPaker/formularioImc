4dde7d190f99acdbaa27df398bbd4ffdded1ef16
MatitodhPaker <hueco35.perez@gmail.com>
2021-03-23 10:33:19 -0600

    se modifico el imc.php

--- imc.php
+++ imc.php
@@ -5,5 +5,14 @@
     $imc=($peso/(pow($altura,2)));
     return $imc;
   }
-  echo calcularImc($peso,$altura);
+  function estas($peso,$altura){
+    $imc=calcularImc($peso,$altura);
+    if ($imc<18 ) {
+      echo "etas desnutrido";
+    } else if($imc>=18 and $imc<24.4) {
+      echo "estas en tu peso normal";
+    }
+    
+  }
+  estas($peso,$altura);
 ?>
